ca1426996bbd4b6a812431c77e8165b2a6f8194d
Douglas Deodanes <deodanes.dbz@gmail.com>
2023-03-21 19:29:24 -0600

    Create model estudiante
    php artisan make:model estudiante -m

--- config/auth.php
+++ config/auth.php
@@ -61,7 +61,7 @@ return [
 
     'providers' => [
         'users' => [
-            'driver' => 'eloquent',
+            'driver' => 'passport',
             'model' => App\Models\User::class,
         ],
 
